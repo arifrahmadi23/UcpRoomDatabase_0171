be6349c17a963fdff8fb3ef5d701faa148b064dc
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-18 10:09:52 +0700

    Membuat Tabel Dosen

---
+++ app/src/main/java/com/example/pertemuan1112/data/entity/Dosen.kt
@@ -0,0 +1,13 @@
+package com.example.pertemuan1112.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "dosen")
+data class Dosen(
+    @PrimaryKey
+    val nidn: String,
+    val nama: String,
+    val jenisKelamin: String
+)
