0ac76ee53de7ab619ad0ae3c42f2701fe9f81935
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-18 10:55:41 +0700

    Membuat ContainerApp

---
+++ app/src/main/java/com/example/pertemuan1112/dependeciesinjection/ContainerApp.kt
@@ -0,0 +1,24 @@
+package com.example.pertemuan1112.dependeciesinjection
+
+import android.content.Context
+import com.example.pertemuan1112.data.database.KrsDatabase
+import com.example.pertemuan1112.repository.LocalRepositoryDsn
+import com.example.pertemuan1112.repository.LocalRepositoryMk
+import com.example.pertemuan1112.repository.RepositoryDsn
+import com.example.pertemuan1112.repository.RepositoryMk
+
+interface InterfaceContainerApp {
+    val RepositoryDsn:RepositoryDsn
+    val RepositoryMk:RepositoryMk
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val RepositoryDsn: RepositoryDsn by lazy {
+        LocalRepositoryDsn(KrsDatabase.getDatabase(context).dosenDao())
+    }
+    override val RepositoryMk: RepositoryMk by lazy {
+        LocalRepositoryMk(KrsDatabase.getDatabase(context).mataKuliahDao())
+    }
+
+
+}
