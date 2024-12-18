cb7cd371dd89938869c1a85c24fb36731ad2025e
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-18 10:20:41 +0700

    Membuat DosenDao (+Fungsi Create dan Fungsi Read)

---
+++ app/src/main/java/com/example/pertemuan1112/data/dao/DosenDao.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan1112.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.pertemuan1112.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface DosenDao {
+
+@Insert
+suspend fun insertDosen(dosen: Dosen)
+
+@Query("SELECT * FROM dosen ORDER BY nama ASC")
+fun getAllDosen(): Flow<List<Dosen>>
+}
