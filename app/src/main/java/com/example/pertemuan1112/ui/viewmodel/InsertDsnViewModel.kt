5c3bfe751cc9b0e1709212bff697abd57ab072c2
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-20 22:54:08 +0700

    Membuat data class DsnUiState dan FormErrorState pada InsertDsnViewModel

--- app/src/main/java/com/example/pertemuan1112/ui/viewmodel/InsertDsnViewModel.kt
+++ app/src/main/java/com/example/pertemuan1112/ui/viewmodel/InsertDsnViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.pertemuan1112.ui.viewmodel
 
+import android.os.Message
 import com.example.pertemuan1112.data.entity.Dosen
 
 //Menyimpan Input dari Form
@@ -15,3 +16,20 @@ fun DosenEvent.toDosenEntity(): Dosen = Dosen(
     nama = nama,
     jenisKelamin = jenisKelamin
 )
+
+data class DsnUiState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
+
+// Memberikan Validasi apakah input form sudah benar atau belum
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null
+){
+    fun isValid(): Boolean{
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
