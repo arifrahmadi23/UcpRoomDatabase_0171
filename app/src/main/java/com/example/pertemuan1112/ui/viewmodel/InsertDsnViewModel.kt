ebc3ac8ccf5b943c6177185b870963f632b670b1
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-20 22:48:28 +0700

    Membuat fungsi DosenEvent.toDosenEntity pada InsertDsnViewModel

--- app/src/main/java/com/example/pertemuan1112/ui/viewmodel/InsertDsnViewModel.kt
+++ app/src/main/java/com/example/pertemuan1112/ui/viewmodel/InsertDsnViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.pertemuan1112.ui.viewmodel
 
+import com.example.pertemuan1112.data.entity.Dosen
+
+//Menyimpan Input dari Form
 data class DosenEvent(
     val nidn: String ="",
     val nama: String ="",
     val jenisKelamin: String=""
+)
+
+// Menyimpan input form ke dalam entity
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin
 )
