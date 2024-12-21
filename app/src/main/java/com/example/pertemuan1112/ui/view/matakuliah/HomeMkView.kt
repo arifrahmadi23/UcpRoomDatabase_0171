c502f1823663fda9964ed8efeedfc69524b705ef
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-21 20:51:32 +0700

    Membuat fungsi ListMataKuliah dan cardMk pada HomeMkView

---
+++ app/src/main/java/com/example/pertemuan1112/ui/view/matakuliah/HomeMkView.kt
@@ -0,0 +1,125 @@
+package com.example.pertemuan1112.ui.view.matakuliah
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pertemuan1112.data.entity.MataKuliah
+
+
+@Composable
+fun ListMataKuliah(
+    listMk: List<MataKuliah>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items = listMk,
+            itemContent = { mk ->
+                CardMk(
+                    mk = mk,
+                    onClick = {onClick(mk.kode)}
+                )
+            }
+        )
+    }
+}
+
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun CardMk(
+    mk: MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+){
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier= Modifier.padding(4.dp))
+                Text(
+                    text = mk.kode,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.sks,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.jenis,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.dosenPengampu,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+
+}
