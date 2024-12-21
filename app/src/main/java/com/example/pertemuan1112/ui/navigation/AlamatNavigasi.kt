7c4dc1aadef84f630f6627972bd8417f580cac54
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-21 16:03:43 +0700

    Menambahkan DestinasiHomeDosen pada AlamatNavigasi

--- app/src/main/java/com/example/pertemuan1112/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan1112/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,7 @@ package com.example.pertemuan1112.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHomeDosen : AlamatNavigasi{
+    override val route = "home_dsn"
+}
