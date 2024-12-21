87963fa0a2317aedfd738ace855158dd8208900b
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-21 16:40:14 +0700

    Membuat fungsi MataKuliahEvent.toMataKuliahEntity untuk menyimpan input form ke dalam entity

--- app/src/main/java/com/example/pertemuan1112/ui/viewmodel/InsertMkViewModel.kt
+++ app/src/main/java/com/example/pertemuan1112/ui/viewmodel/InsertMkViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.pertemuan1112.ui.viewmodel
 
+import com.example.pertemuan1112.data.entity.MataKuliah
+
 data class MataKuliahEvent(
     val kode: String = "",
     val nama: String = "",
@@ -7,4 +9,13 @@ data class MataKuliahEvent(
     val semester: String = "",
     val jenis: String = "",
     val dosenPengampu: String = ""
+)
+
+fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
+    kode = kode,
+    nama = nama,
+    sks = sks,
+    semester = semester,
+    jenis = jenis,
+    dosenPengampu = dosenPengampu
 )
