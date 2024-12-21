d6061dbcf8731f219b3a9919f6b544c8815445b3
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-21 21:27:57 +0700

    Membuat ComponentDetailMk pada DetailMkView

---
+++ app/src/main/java/com/example/pertemuan1112/ui/view/matakuliah/DetailMkView.kt
@@ -0,0 +1,35 @@
+package com.example.pertemuan1112.ui.view.matakuliah
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ComponentDetailMk(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+)
+{
+    Column (modifier = modifier.fillMaxWidth(),
+
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
