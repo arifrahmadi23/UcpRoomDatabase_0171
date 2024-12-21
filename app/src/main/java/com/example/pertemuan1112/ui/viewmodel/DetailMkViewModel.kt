e9084404c6dc7a0c7a8deb70cef78e0417817814
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-21 19:40:55 +0700

    Membuat class DetailMkViewModel

--- app/src/main/java/com/example/pertemuan1112/ui/viewmodel/DetailMkViewModel.kt
+++ app/src/main/java/com/example/pertemuan1112/ui/viewmodel/DetailMkViewModel.kt
@@ -2,14 +2,61 @@ package com.example.pertemuan1112.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan1112.data.entity.MataKuliah
 import com.example.pertemuan1112.repository.RepositoryMk
+import com.example.pertemuan1112.ui.navigation.DestinasiDetailMk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DetailMkViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMk: RepositoryMk,
 ) : ViewModel(){
-    private val _kode: String = checkNotNull(savedStateHandle[DestinasiDetail.kode])
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiDetailMk.KODE])
+
+    val detailUiStateMk: StateFlow<DetailUiStateMk> = repositoryMk.getMk(_kode)
+        .filterNotNull()
+        .map {
+            DetailUiStateMk(
+                detailUiEventMk = it.toDetailUiEvent(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(DetailUiStateMk(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiStateMk(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiStateMk(
+                isLoading = true,
+            ),
+        )
+    fun deleteMk(){
+        detailUiStateMk.value.detailUiEventMk.toMataKuliahEntity().let {
+            viewModelScope.launch {
+                repositoryMk.deleteMk(it)
+            }
+        }
+    }
 }
 
 
