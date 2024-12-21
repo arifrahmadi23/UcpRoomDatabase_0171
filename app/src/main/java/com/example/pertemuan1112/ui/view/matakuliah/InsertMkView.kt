a994f51aff4cf5072cf9fdbe88c1896eb64002f5
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-21 21:21:43 +0700

    Membuat Form MataKuliah (- DosenPengampu)

---
+++ app/src/main/java/com/example/pertemuan1112/ui/view/matakuliah/InsertMkView.kt
@@ -0,0 +1,132 @@
+package com.example.pertemuan1112.ui.view.matakuliah
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pertemuan1112.ui.viewmodel.FormErrorStateMk
+import com.example.pertemuan1112.ui.viewmodel.MataKuliahEvent
+
+@Composable
+fun FormMataKuliah(
+    mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+    onValueChange: (MataKuliahEvent) -> Unit,
+    errorState: FormErrorStateMk = FormErrorStateMk(),
+    modifier: Modifier = Modifier
+) {
+
+    val jenis = listOf("Wajib", "Pilihan")
+    
+    Column (
+        modifier = modifier.fillMaxWidth()
+    ){
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.nama,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan nama mata kuliah") },
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.kode, onValueChange = {
+                onValueChange(mataKuliahEvent.copy(kode = it))
+            },
+            label = { Text("Kode") },
+            isError = errorState.kode != null,
+            placeholder = { Text("Masukkan Kode Matakuliah") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.kode ?: "", color = Color.Red)
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.sks, onValueChange = {
+                onValueChange(mataKuliahEvent.copy(sks = it))
+            },
+            label = { Text("SKS") },
+            isError = errorState.sks != null,
+            placeholder = { Text("Masukkan SKS") },
+        )
+        Text(text = errorState.sks ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.semester, onValueChange = {
+                onValueChange(mataKuliahEvent.copy(semester = it))
+            },
+            label = { Text("Semester") },
+            isError = errorState.semester != null,
+            placeholder = { Text("Masukkan Semester") },
+
+            )
+        Text(text = errorState.semester ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Jenis")
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            jenis.forEach { jk ->
+                Row (verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    RadioButton(
+                        selected = mataKuliahEvent.jenis == jk,
+                        onClick = {
+                            onValueChange(mataKuliahEvent.copy(jenis = jk))
+                        },
+                    )
+                    Text(text = jk)
+                }
+            }
+        }
+        Text(text = errorState.jenis ?: "",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Pilih Dosen Pengampu", fontWeight = FontWeight.Bold)
+        Text(
+            text = "Silahkan pilih Dosen Pengampu",
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Light
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        DynamicSelectTextField(
+            selectedValue = chosenDropdown,
+            options = MataKuliah.options,
+            label = "Mata Kuliah",
+            onValueChangedEvent = {
+                chosenDropdown = it
+            }
+        )
+    }
+}
