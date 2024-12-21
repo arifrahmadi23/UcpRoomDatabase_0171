0d821e40e528aa3c60f1b5522ec3b53667702dc6
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-21 16:38:12 +0700

    Membuat data class MataKuliahEvent pada InserMkViewModel

---
+++ app/src/main/java/com/example/pertemuan1112/ui/viewmodel/InsertMkViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan1112.ui.viewmodel
+
+data class MataKuliahEvent(
+    val kode: String = "",
+    val nama: String = "",
+    val sks: String = "",
+    val semester: String = "",
+    val jenis: String = "",
+    val dosenPengampu: String = ""
+)
